d58b59aa8c78c6e27c5267b9b9e7724986661873
Martin Björkström <martin@bjorkstrom.dev>
2026-04-01 12:37:33 +0300

    Revert "feat: ship MSBuild target to filter .azurefunctions DLLs at build time"
    This reverts commit 5c3b2f6339d7df2548a62199a900e84d6b00fb06.

--- tests/Sample.FunctionApp.CustomRoutePrefix.AspNetCore.DifferentSdk.Tests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Net;
-global using System.Net.Http.Json;
-global using System.Text.Json;
-global using AzureFunctions.TestFramework.Core;
-global using Sample.FunctionApp.CustomRoutePrefix.AspNetCore;
-global using Xunit;
-global using Xunit.Abstractions;
